138f9a602c92c010b3a739b581d65ce0b3afdb30
Galadriel <galadriel.briere@gmail.com>
2024-10-14 14:35:20 +0200

    runs

---
+++ models_training/minimal_kg/mixed_test/unpermuted/ANALOGY/gpu_run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:1g.5gb:1
+#SBATCH --time 3-00:00:00
+#SBATCH --job-name=ANALOGY
+#SBATCH --output=ANALOGY_%j.txt
+#SBATCH --ntasks=1
+#SBATCH --mem=20G
+
+module load conda
+
+conda activate drug_repurposing_benchmark
+
+python /shared/projects/ml_het_bio_nets/drug_repurposing_kge_benchmark/dev/run_training.py \
+    --config /shared/projects/ml_het_bio_nets/drug_repurposing_kge_benchmark/models_training/minimal_kg/mixed_test/unpermuted/ANALOGY/params.yaml \
+    2>&1 | tee /shared/projects/ml_het_bio_nets/drug_repurposing_kge_benchmark/models_training/minimal_kg/mixed_test/unpermuted/ANALOGY/out.log
