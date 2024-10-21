7f215344643096127911419ea41ae7632c11c9ee
Galadriel <galadriel.briere@gmail.com>
2024-10-21 18:04:39 +0200

    models params experiment1é

---
+++ experiment1_user_induced_DL/induced_DL/models_training/ComplEx/run1/gpu_run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH -A rnk@v100
+#SBATCH --job-name=e1_ComplEx_inducedDL
+#SBATCH --gres=gpu:1
+#SBATCH --time=20:00:00
+#SBATCH --output=e1_ComplEx_inducedDL_%j.log
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=3
+#SBATCH --hint=nomultithread
+
+module load python
+conda deactivate
+conda activate torch_pyg
+
+cd $WORK/dr_benchmark/experiment1_user_induced_DL/induced_DL/models_training/ComplEx/run1
+
+srun python $WORK/dr_benchmark/dev/run_training.py \
+    --config params.yaml 
