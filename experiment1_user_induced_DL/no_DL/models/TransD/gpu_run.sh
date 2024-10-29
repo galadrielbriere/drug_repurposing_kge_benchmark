98945fbe176975a4b9c61d75d82fb981be3b631e
Galadriel <galadriel.briere@gmail.com>
2024-10-29 10:22:38 +0100

    TransD

---
+++ experiment1_user_induced_DL/no_DL/models/TransD/gpu_run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH -A rnk@v100
+#SBATCH --job-name=e1r1nodl_TransD
+#SBATCH --gres=gpu:1
+#SBATCH --time=100:00:00
+#SBATCH --output=e1r1nodl_TransD_%j.log
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=5
+#SBATCH --hint=nomultithread
+#SBATCH -C v100-32g
+#SBATCH --qos=qos_gpu-t4
+
+module load python
+conda deactivate
+conda activate torch_pyg
+
+cd $WORK/dr_benchmark/experiment1_user_induced_DL/no_DL/models/TransD/
+
+srun python $WORK/dr_benchmark/dev/run_training.py \
+    --config params.yaml 
