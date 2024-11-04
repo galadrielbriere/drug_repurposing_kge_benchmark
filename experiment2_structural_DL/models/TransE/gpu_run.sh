5ea56223d883757d22731a9d2dd5da2b267336cc
Galadriel <galadriel.briere@gmail.com>
2024-11-04 14:33:06 +0100

    perm exp

---
+++ experiment2_structural_DL/models/TransE/gpu_run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH -A rnk@v100
+#SBATCH --job-name=e2r1p_TransE
+#SBATCH --gres=gpu:1
+#SBATCH --time=20:00:00
+#SBATCH --output=e2r1p_TransE_%j.log
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=5
+#SBATCH --hint=nomultithread
+#SBATCH -C v100-32g
+##SBATCH --qos=qos_gpu-t4
+
+module load python
+conda deactivate
+conda activate torch_pyg
+
+cd $WORK/dr_benchmark/experiment2_structural_DL/models/TransE/
+
+srun python $WORK/dr_benchmark/dev/run_training.py \
+    --config params.yaml 
