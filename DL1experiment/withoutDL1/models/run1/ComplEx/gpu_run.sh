e5a31d7d6bcc271c66a0d1e8dbab9ec7dded9b20
Galadriel <galadriel.briere@gmail.com>
2024-12-31 15:19:30 +0100

    rerun complex

---
+++ DL1experiment/withoutDL1/models/run1/ComplEx/gpu_run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH -A rnk@v100
+#SBATCH --job-name=e1r1nodl_ComplEx
+#SBATCH --gres=gpu:1
+#SBATCH --time=20:00:00
+#SBATCH --output=e1r1nodl_ComplEx_%j.log
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=3
+#SBATCH --hint=nomultithread
+#SBATCH -C v100-32g
+##SBATCH --qos=qos_gpu-t4
+
+module load python
+conda deactivate
+conda activate torch_pyg
+
+cd $WORK/dr_benchmark/DL1experiment/withoutDL/models/run1/ComplEx/
+
+srun python $WORK/dr_benchmark/dev/run_training.py \
+    --config params.yaml 
